fba96f0416b20d6abb5e93bc6f70b72c6ac9e279
MuhammadIshaq70 <muhammadishaq2921217@gmail.com>
2023-09-27 17:43:38 +0500

    1.2

--- lib/src/UI/screens/selectRide/selectRide.dart
+++ lib/src/UI/screens/selectRide/selectRide.dart
@@ -141,16 +141,6 @@ class _SelectRideState extends State<SelectRide> {
                         Text("data"),
                       ],
                     ),
-                    ExpansionTile(
-                      title: Text("Inner data"),
-                      children: [
-                        Text("data"),
-                        Text("data"),
-                        Text("data"),
-                        Text("data"),
-                        Text("data"),
-                      ],
-                    ),
                   ],
                 )
               ],
